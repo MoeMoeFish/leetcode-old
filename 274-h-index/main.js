69e3f0ae49b10344a396603135f451f4bbc97ebf
ThirteenFish <sighforever@gmail.com>
2015-09-08 13:17:29 +0800

    274 complete not test

---
+++ 274-h-index/main.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function( citations ) {
+    var buffer = [];
+    var length = citations.length;
+    var retVal = 0;
+    
+    for ( var i = 0; i <= length; i++ ) {
+        buffer[ i ] = 0;
+    }
+        
+    for ( var i = 0;i < length; i++ ) {
+        var h = citations[i]; 
+        if ( h < 0 ) {
+            ++buffer[ 0 ]; 
+        } else if ( h > length ) {
+            ++buffer[ length ]; 
+        } else {
+            ++buffer[ h ];
+        }
+    }
+    
+    var total = 0;
+    for ( var i = Number(length); i >= 0; i-- ) {
+        total += buffer[i];
+        
+        if ( buffer[i] > 0 && total >= i ) {
+            return i;
+        }
+    }
+    
+    return retVal;
+};
+
+
+module.exports = hIndex;
