d48cea72d2bde84c855d46649a7ac72db8cbdf1e
ThirteenFish <sighforever@gmail.com>
2015-09-15 19:32:43 +0800

    264 begin

---
+++ 264-Ugly-Number-II/test.js
@@ -0,0 +1,16 @@
+var test = require( 'tape' );
+var nthUglyNumber = require( './main' );
+
+test( 'Ugly Number II', function( t ) {
+	t.equal(nthUglyNumber(1), 1, '1');
+	t.equal(nthUglyNumber(2), 2, '2');
+	t.equal(nthUglyNumber(3), 3, '3');
+	t.equal(nthUglyNumber(4), 4, '4');
+	t.equal(nthUglyNumber(5), 5, '5');
+	t.equal(nthUglyNumber(6), 6, '6');
+	t.equal(nthUglyNumber(7), 8, '8');
+	t.equal(nthUglyNumber(8), 9, '9');
+	t.equal(nthUglyNumber(9), 10, '10');
+	t.equal(nthUglyNumber(10), 12, '11');
+	t.end();
+} );
