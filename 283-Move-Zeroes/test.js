83a2f1e6000a327fe2a13a8ca7cc5ca2cee075c7
ThirteenFish <sighforever@gmail.com>
2015-10-08 19:37:34 +0800

    equal -> deep equal

--- 283-Move-Zeroes/test.js
+++ 283-Move-Zeroes/test.js
@@ -3,6 +3,8 @@ var test = require( 'tape' );
 
 
 test( 'Move Zeroes', function( t ) {
-	t.equal(  mz([0, 1, 0, 3, 12]),  [1, 3, 12, 0, 0], ' [0, 1, 0, 3, 12]' );
+	var nums = [0, 1, 0, 3, 12];
+	mz( nums );
+	t.deepEqual(  nums,  [1, 3, 12, 0, 0], ' [0, 1, 0, 3, 12]' );
 	t.end();
 } );
