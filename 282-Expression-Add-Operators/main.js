8aee66d9b7a32438d2d03b40d08cd44de3de5889
ThirteenFish <sighforever@gmail.com>
2015-10-15 13:00:25 +0800

    282 test pass

--- 282-Expression-Add-Operators/main.js
+++ 282-Expression-Add-Operators/main.js
@@ -3,8 +3,54 @@
  * @param {number} target
  * @return {string[]}
  */
-var addOperators = function(num, target) {
+var addOperators = function( num, target ) {
+    var result= [];
     
+    if ( num.length == 0 ) {
+        return result;    
+    }
+    
+    //var number1 = parseInt( num.substr( 0 , 1 ))
+    helper( num, target, result, '', 0, 0, 0);
+    
+    return result;
 };
+
+/**
+ * @param {int} num
+ * @param {number} target
+ * @param {number} target
+ * @param {string[]} result
+ * @param {string} record
+ * @param {number} position
+ * @param {number} pre
+ * @param {number} multipled
+ * @return {void}
+ */
+var helper = function( num, target, result, record, position, pre, multipled ) {
+    //console.log( 'record = ' + record + ' pre = ' + pre );
+    if ( position === num.length && pre === target ) {
+        result.push( record );
+        return;
+    }
+    
+    for (var i = position; i < num.length; i++ ) {
+        if ( i != position && num[position] === '0' ) {
+            break;
+        }
+        
+        var next = parseInt( num.substr(position, i - position + 1 ) );
+        //console.log( 'next = ' + next + ' pre = ' + pre );
+        if ( position === 0 ) {
+            helper( num, target, result, next.toString(), i + 1, next, next );
+            continue;
+        }
+        
+        
+        helper( num, target, result, record + '+' + next, i + 1, pre + next, next );
+        helper( num, target, result, record + '-' + next, i + 1, pre - next, next * -1 );
+        helper( num, target, result, record + '*' + next, i + 1, ( pre - multipled ) + ( multipled * next ) , multipled * next );
+    }
+}
 
 module.exports = addOperators;
--- 282-Expression-Add-Operators/test.js
+++ 282-Expression-Add-Operators/test.js
@@ -1,3 +1,16 @@
 var eao = require( './main.js' );
 var test = require( 'tape' );
 
+// test( 'Expression Add Operator', function( t ) {
+// 	t.deepEqual( eao( "123", 6 ), ["1+2+3", "1*2*3"] );
+// 	t.end();
+// } );
+
+test( 'Expression Add Operator', function( t ) {
+	t.deepEqual( eao( "123", 6 ), ["1+2+3", "1*2*3"] );
+	t.deepEqual( eao( "232", 8 ), ["2*3+2", "2+3*2"] );
+	t.deepEqual( eao( "105", 5 ), ["1*0+5","10-5"] );
+	t.deepEqual( eao( "00", 0 ), ["0+0", "0-0", "0*0"] );
+	t.deepEqual( eao( "3456237490", 9191 ), [] );
+	t.end();
+} );
