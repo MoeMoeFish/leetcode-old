ab322fdf25fe31838b59e6fab27622767046542b
Fish Thirteen <fishthirteen@qq.com>
2017-07-15 21:38:24 +0800

    rename

---
+++ 479-Largest-Palindrome-Product/main.js
@@ -0,0 +1,16 @@
+var largestPalindrome = function(n) {
+};
+
+var isPalidrome = function( n ) {
+  var a = n;
+  var h = 1;
+
+  while ( a / h >= 10 ) {
+    h *= 10;
+  }
+
+  
+}
+
+
+module.exports = largestPalindrome;
--- 479-Largest-Palindrome-Product/test.js
+++ 479-Largest-Palindrome-Product/test.js
@@ -1,4 +1,9 @@
 let func = require('./main.js');
 let test = require('tape');
 
+test( 'Largest Palindrome Product', function( t ) {
+  t.equal( 987, func( 2 ) );
+  t.end();
+} );
+
 
