0cbfb2970be2f6c1ebb9c63e62f71525465f47a2
yuFish <fish@yuFishs-MBP.lan>
2018-03-12 00:37:34 +0800

    0008 improve

--- 0008-String-To-Integer/main.js
+++ 0008-String-To-Integer/main.js
@@ -4,49 +4,27 @@
  */
 const INT_MAX = 0x7FFFFFFF
 const INT_MIN = -0x80000000
-const INT_DICT = {
-    '0': true,
-    '1': true,
-    '2': true,
-    '3': true,
-    '4': true,
-    '5': true,
-    '6': true,
-    '7': true,
-    '8': true,
-    '9': true
-}
 
 var myAtoi = function(str) {
     let rv = 0
 
-    let begin = false
     let sign = 1
 
-    for (let i = 0;i < str.length;i++) {
+    let i = 0
+    while(str[i] === ' ') {
+        i++
+    }
+
+    if (str[i] === '+' || str[i] === '-') {
+        sign = str[i] === '+' ? 1 : -1
+        i++
+    }
+
+    for (;i < str.length;i++) {
         let char = str[i]
 
-        if (char === ' ') {
-            if (begin) {
-                break
-            }
-
-            continue
-        }
-        
-        if (char === '+' || char === '-') {
-            if (begin) {
-                break
-            }
-
-            begin = true
-            sign = char === '+' ? 1 : -1
-            continue
-        }
-
-        if (INT_DICT[char]) {
-            begin = true
-            let num = char.charCodeAt(0) - 48
+        if (char.charCodeAt() >= '0'.charCodeAt() && char.charCodeAt() <= '9'.charCodeAt()) {
+            let num = char.charCodeAt(0) - '0'.charCodeAt()
             rv = rv * 10 + sign * num 
 
             if (rv > INT_MAX) {
