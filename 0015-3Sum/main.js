61eff56e97ccf0103084fb06c0b72c8b39d283ee
MoeMoeFish <moemoefish@qq.com>
2019-04-18 00:20:05 +0800

    0015

--- 0015-3Sum/main.js
+++ 0015-3Sum/main.js
@@ -7,40 +7,38 @@ var threeSum = function(nums) {
         return []
     }
 
-    nums = nums.sort()
+    nums = nums.sort((a, b) => {
+        return a - b
+    })
     let rv = []
 
-    for (let i = 0;i < nums.length - 1;i++) {
+    for (let i = 0; i < nums.length - 2; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) {
             continue
         }
 
         let target = 0 - nums[i]
-
-        if (target < 0) {
-            return rv
-        }
-
         let j = i + 1, k = nums.length - 1
-        while(j < k) {
-            console.log('#', i, j, k, nums[i], nums[j], nums[k])
-            while (k < nums.length - 1 && nums[k] === nums[k + 1]) {
-                k--
-            }
-            while (j > i + 1 && nums[j] === nums[j - 1]) {
-                j++
-            }
-            if (nums[j] + nums[k] > target) {
-                k--
-            } else if (nums[j] + nums[k] < target) {
-                j++
-            } else {
-                console.log('@', i, j, k, nums[i], nums[j], nums[k])
+
+        while (j < k) {
+            if (nums[j] + nums[k] === target) {
                 rv.push([nums[i], nums[j], nums[k]])
                 j++
+                while (nums[j] === nums[j - 1]) {
+                    j++
+                }
+                k--
+                while (nums[k] === nums[k + 1]) {
+                    k--
+                }
+                continue;
+            }
+            while (nums[j] + nums[k] > target && j < k) {
                 k--
             }
-
+            while (nums[j] + nums[k] < target && j < k) {
+                j++
+            }
         }
     }
 
